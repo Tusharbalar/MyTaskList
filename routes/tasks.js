2897d770791fd6bef856250ae49262d6b4f3f15b
TusharBalar <balar.tushar1@gmail.com>
2017-03-15 01:34:58 +0530

    Impliment findOne() method to get single data

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,13 +3,23 @@ var router = express();
 var mongojs = require("mongojs");
 var db = mongojs("mongodb://tushar:tushar@ds129030.mlab.com:29030/mytasks", ["tasks"]);
 
+// GET ALL TASKS
 router.get("/tasks", function(req, res, next) {
   db.tasks.find(function(err, tasks) {
     if (err) {
       res.send(err);
     }
-
     res.json(tasks);
+  })
+});
+
+// GET SINGLE TASK
+router.get("/tasks/:id", function(req, res, next) {
+  db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(task);
   })
 });
 
